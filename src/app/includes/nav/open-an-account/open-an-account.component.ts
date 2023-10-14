8bbd70f6e5dd3434b200c4eb25358963884b5609
Vinay Khatri <khatrivinay520@gmail.com>
2023-10-14 23:11:25 +0530

    Changes

--- src/app/includes/nav/open-an-account/open-an-account.component.ts
+++ src/app/includes/nav/open-an-account/open-an-account.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-open-an-account',
   templateUrl: './open-an-account.component.html',
   styleUrls: ['./open-an-account.component.css']
 })
 export class OpenAnAccountComponent {
-
+  constructor(private formBuilder: FormBuilder) {
+    this.formDetails = this.formBuilder.group({
+      name: new FormControl(
+        '',
+        Validators.required
+      ),
+      // contact: new FormControl(
+      //   '',
+      //   Validators.required
+      // ),
+      email: new FormControl(
+        '',
+        [Validators.email, Validators.required]
+      ),
+      // age: new FormControl(
+      //   '',
+      //   [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
+      // ),
+      // qualification: new FormControl(
+      //   '',
+      //   Validators.required  
+      // )
+    });
+  }
+  formDetails:any;
+  submit(){
+    console.log(this.formDetails.value)
+  }
 }
